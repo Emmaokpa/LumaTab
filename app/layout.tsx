0c1bb388b240bb82dd164aedacfb3fd6f26897c8
eokpa34 <eokpa34@gmail.com>
2025-09-24 10:59:00 +0000

    providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`} suppressHydrationWarning>
+      <body className="font-sans">
         <Providers>
           <Suspense fallback={null}>{children}</Suspense>
         </Providers>
